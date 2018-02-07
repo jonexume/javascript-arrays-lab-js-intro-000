5f92cb416251b905e188c11ef1847e83e0b08466
Jon Exume <jonexume@gmail.com>
2018-02-07 05:35:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 function destructivelyAppendKitten() {
   var kittens = ["Milo", "Otis", "Garfield"];
   kittens.push("Ralph");
-  return kittens;
+  console.log(kittens);
 }
 
 function destructivelyPrependKitten() {
